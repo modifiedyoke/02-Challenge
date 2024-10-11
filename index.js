1e63d431b303b449b5f9799a763219a4728cfb09
modifiedyoke <bshurts@outlook.com>
2024-10-10 20:21:39 -0500

    saving work

--- index.js
+++ index.js
@@ -1,13 +1,61 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+
+let answers;
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: 'title',
+        message: 'Project Title:',
+    },
+    {
+        name: 'description',
+        message: 'Description:',
+    },
+    {
+        name: 'installation',
+        message: 'Installation Instructions:',
+    },
+    {
+        name: 'usage',
+        message: 'Usage Information:',
+    },
+    {
+        name: 'contribute',
+        message: 'Contribution Guidelines:',
+    },
+    {
+        name: 'test',
+        message: 'Test Instructions:',
+    },
+    {
+        name: 'license',
+        message: 'Choose a license:',
+        type: 'list',
+        choices: ['MIT', 'GPLv3', 'none'],
+    },
+    {
+        name: 'profile',
+        message: 'Enter your Github username: ',
+    },
+    {
+        name: 'email',
+        message: 'Enter your email address: ',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(answers)
+    .then((answers) => {
+        console.log(answers);
+    }
+    );
+}
 
 // Function call to initialize app
 init();
